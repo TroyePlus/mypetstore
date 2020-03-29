7da99ef797094ab8fe4f125e78a6e9354ef79635
Peachck <44659673+Peachck@users.noreply.github.com>
2020-03-29 14:29:29 +0800

    页面修改完成

--- src/test/java/org/csu/mypetstore/MypetstoreApplicationTests.java
+++ src/test/java/org/csu/mypetstore/MypetstoreApplicationTests.java
@@ -1,8 +1,5 @@
 package org.csu.mypetstore;
 
-import org.csu.mypetstore.domain.Category;
-import org.csu.mypetstore.service.CategoryService;
-import org.junit.jupiter.api.Test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,18 +10,6 @@ import java.util.Calendar;
 @SpringBootTest
 @MapperScan("org.csu.mypetstore.persistence")
 class MypetstoreApplicationTests {
-    @Autowired
-    private CategoryService categoryService;
 
-    @Test
-    void contextLoads() {
-    }
-
-    @Test
-    void testCategory(){
-        Category category = categoryService.getCategory("BIRDS");
-        System.out.println(category.getCategoryId() +" "+ category.getDescription());
-
-    }
 
 }
